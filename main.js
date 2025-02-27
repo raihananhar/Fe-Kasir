f7c02afad1adc0f0b8219aa5b7d36b96a331e5f8
raihananhar <raihanannr@gmail.com>
2025-02-27 16:40:10 +0700

    langkah pertama

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('index.html'); // Nanti ganti ke UI kasir
+});
